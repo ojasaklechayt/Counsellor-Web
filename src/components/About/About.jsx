0494dcdf75611ebfe2e9c96c911b38d75168ccf4
Ojas Aklecha <ojasaklechayt@gmail.com>
2023-06-21 20:31:23 +0530

    Added About Content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,18 @@
 import Navbar from "../Navbar/Navbar";
-
+import "./About.css";
 const About = () => {
     return(
         <>
         <Navbar />
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nemo cupiditate laborum dignissimos
-             illum, nisi iure delectus? Placeat dignissimos ex temporibus quia! Ratione aut voluptas 
-             molestiae ipsa minima repudiandae beatae.
+        <div className="about">
+            <div className="title"><h1>About Us</h1></div>
+            <h3>A platform that helps in counseling students for their college could be a powerful resource for students and counselors alike. Here are some key features that such a platform might offer:</h3>
+            <h4>1. The platform could provide college search tools with filters for location, size, majors, campus culture, and other relevant factors. This would help students find colleges that match their interests, needs, and goals..</h4>
+            <h4>2. College Search Tools: The platform could offer a range of search tools to help students find colleges that match their interests, needs, and goals. This might include filters for location, size, majors offered, campus culture, and other relevant factors.</h4>
+            <h4>3. Career Exploration Resources: The platform could offer resources to help students explore different career paths and identify areas of interest. This might include personality assessments, career assessments, and other tools to help students discover their strengths and passions.</h4>
+            <h4>4. College Application Assistance: The platform could offer resources to help students navigate the college application process, including guidance on writing essays, filling out applications, and preparing for interviews. This might include access to sample essays, application checklists, and other helpful tools.</h4>
+            <h4>5. Financial Aid Information: The platform could offer information about financial aid options, including scholarships, grants, and loans, to help students make informed decisions about how to finance their education. This might include a financial aid calculator to help students estimate their costs and potential aid packages.</h4>
+            <h4>6. Student Reviews and Ratings: The platform could provide access to reviews and ratings of colleges and universities from current and former students. This could help students get a better sense of what life is like on campus and what the student experience is like.</h4>
         </div>
         </>
     )
